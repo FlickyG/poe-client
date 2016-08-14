c2b40f96150cd3626f72403da880a4326a1dc410
adam.green <adam.green@ocado.com>
2016-08-14 23:41:46 +0100

    now writes top level types to database

--- get-generic-data.py
+++ get-generic-data.py
@@ -13,10 +13,74 @@ logger.debug("hello world")
 print(__name__)
 
 def write_weapon_types(list):
+    logger.debug("entering write_weapon_types (%s)", list)
     connQ = psycopg2.connect("dbname='poe_data'  user='adam' password='green'")
     currQ = connQ.cursor()
-    
+    for x in list:
+        try:
+            currQ.execute("INSERT INTO weapon_types (type) "
+                        "VALUES (%s)",
+             [x])           
+            connQ.commit()
+        except psycopg2.IntegrityError:
+            logger.debug("psql integrity error when commiting weapons types type (%s)", x)
+            connQ.rollback()
 
+def write_clothing_types(list):
+    logger.debug("entering write_clothing_types (%s)", list)
+    connQ = psycopg2.connect("dbname='poe_data'  user='adam' password='green'")
+    currQ = connQ.cursor()
+    for x in list:
+        try:
+            currQ.execute("INSERT INTO clothing_types (type) "
+                        "VALUES (%s)",
+             [x])           
+            connQ.commit()
+        except psycopg2.IntegrityError:
+            logger.debug("psql integrity error when commiting clothing types type (%s)", x)
+            connQ.rollback()  
+    
+def write_jewelry_types(list):
+    logger.debug("entering write_jewelry_types (%s)", list)
+    connQ = psycopg2.connect("dbname='poe_data'  user='adam' password='green'")
+    currQ = connQ.cursor()
+    for x in list:
+        try:
+            currQ.execute("INSERT INTO jewelry_types (type) "
+                        "VALUES (%s)",
+             [x])           
+            connQ.commit()
+        except psycopg2.IntegrityError:
+            logger.debug("psql integrity error when commiting jewelry types type (%s)", x)
+            connQ.rollback()
+    
+def write_prefix_types(list):
+    logger.debug("entering write_prefix_types (%s)", list)
+    connQ = psycopg2.connect("dbname='poe_data'  user='adam' password='green'")
+    currQ = connQ.cursor()
+    for x in list:
+        try:
+            currQ.execute("INSERT INTO prefix_types (type) "
+                        "VALUES (%s)",
+             [x])           
+            connQ.commit()
+        except psycopg2.IntegrityError:
+            logger.debug("psql integrity error when commiting prefix types type (%s)", x)
+            connQ.rollback()
+            
+def write_suffix_types(list):
+    logger.debug("entering write_suffix_types (%s)", list)
+    connQ = psycopg2.connect("dbname='poe_data'  user='adam' password='green'")
+    currQ = connQ.cursor()
+    for x in list:
+        try:
+            currQ.execute("INSERT INTO suffix_types (type) "
+                        "VALUES (%s)",
+             [x])           
+            connQ.commit()
+        except psycopg2.IntegrityError:
+            logger.debug("psql integrity error when commiting suffix types type (%s)", x)
+            connQ.rollback()  
 
 def make_throttle_hook(timeout=1.0):  # for eve market api calls
     """
@@ -123,7 +187,6 @@ def parse_clothing(item_data):
             item["implicit_mod_key_"+str(x)] = mod_keys[x]
             item["implicit_mod_values_"+str(x)+"_min"] = mod_values[x][0]
             item["implicit_mod_values_"+str(x)+"_max"] = mod_values[x][1]
-        print(item) 
     except IndexError: # not all items have implicit mods
         pass
     
@@ -136,9 +199,6 @@ def parse_jewelry(item_data):
     #<td>Zombie Quantity<br/>Base Number Of Skeletons Allowed<br/>Spectre Quantity<br/>Local Stat Monsters Pick Up Item<br/></td>
     key_results = [ x for x in re.findall(r">(.*?)<",str(item_data[0][3])) if x]
     value_results = [ x for x in re.findall(r">(.*?)<",str(item_data[0][4])) if x]
-    print(key_results)
-    print(value_results)
-    print("raw values", str(item_data[0][4]))
     mod_keys = []
     mod_values = []
     for x in key_results:
@@ -146,11 +206,8 @@ def parse_jewelry(item_data):
             pass # skip these
         else:
             mod_keys.append(x)
-    print("key_results", key_results)
-    print("value_results", value_results)
     # find implicit values
     if item["name"] == "Undying Flesh Talisman":
-            print("$$$ item detected $$$")
             mod_values = [['0','0'],['1', '1'],['1','1'],['0','0']]   
     else:
         for x in value_results:
@@ -168,14 +225,13 @@ def parse_jewelry(item_data):
                 these_values.append(min_value)
                 these_values.append(max_value)
                 mod_values.append(these_values)
-    print("lengths", len(mod_keys),len(mod_values), mod_keys, mod_values)
     assert len(mod_keys) == len(mod_values) # sense check we're passing these correctly
     number_of_mods = len(mod_keys)
     for x in range(0,number_of_mods):
         item["implicit_mod_key_"+str(x)] = mod_keys[x]
         item["implicit_mod_values_"+str(x)+"_min"] = mod_values[x][0]
         item["implicit_mod_values_"+str(x)+"_max"] = mod_values[x][1]
-    print(item)         
+      
 
 def parse_prefixes(item_data):
     #item_data [[<td class="name">Electrocuting</td>, <td>74</td>, <td>Spell Minimum Added Lightning Damage<br/>Spell Maximum Added Lightning Damage</td>, <td>5 to 15<br/>189 to 200</td>]]
@@ -220,6 +276,7 @@ def parse_prefixes(item_data):
         item["implicit_mod_key_"+str(x)] = key_results[x]
         item["implicit_mod_values_"+str(x)+"_min"] = mod_values[x][0]
         item["implicit_mod_values_"+str(x)+"_max"] = mod_values[x][1]
+    return(item)
         
 def parse_suffixes(item_data):
     #item_data [[<td class="name">Electrocuting</td>, <td>74</td>, <td>Spell Minimum Added Lightning Damage<br/>Spell Maximum Added Lightning Damage</td>, <td>5 to 15<br/>189 to 200</td>]]
@@ -260,9 +317,8 @@ def parse_suffixes(item_data):
         for x in range(0,number_of_mods):
             item["implicit_mod_values_"+str(x)+"_min"] = mod_values[x][0]
             item["implicit_mod_values_"+str(x)+"_max"] = mod_values[x][1]
-    print(item)
 
-def get_weapons():
+def fetch_weapons():
     weapon_types = []
     resp = s.get(url_weap)
     soup = BeautifulSoup(resp.text, 'html.parser')
@@ -271,14 +327,12 @@ def get_weapons():
     weapons[0].find_all("td", {"class": "name"})
     for y in weapons:
         a = y.find_all("td", {"class": "name"})
-        #print (a[0].text)
-    print ("### END OF PRINTIONG ALL WEAPON NAMES ###")        
+        #print (a[0].text) 
     all_items = soup.find_all("div", {"class": "layoutBox1 layoutBoxFull defaultTheme"})    
-    #print all item typyes
     for item_type in all_items:
         w_type = item_type.find_all("h1", {"class": "topBar last layoutBoxTitle"})[0].text #gets all item catagory names
-        print(w_type)
         weapon_types.append(w_type)
+        #write_weapon_types(weapon_types)
         items = item_type.find_all("table", {"class": "itemDataTable"}) #gets ALL the raw data for each item class
         for item_data in items: # for each weaspon class
             data = item_data.find_all("tr") #get the raw data
@@ -295,7 +349,8 @@ def get_weapons():
                 x = x+1
     write_weapon_types(weapon_types)
 
-def get_clothes():
+def fetch_clothes():
+    clothing_types = []
     resp = s.get(url_clothes)
     soup = BeautifulSoup(resp.text, 'html.parser')
     clothes = soup.find_all("tr") 
@@ -304,13 +359,12 @@ def get_clothes():
         a = y.find_all("td", {"class": "name"})
         if len(a) > 0:
             pass
-            #print (a[0].text)
-    print ("### END OF PRINTIONG ALL CLOTHING NAMES ###")
-        
+            #print (a[0].text)        
     all_items = soup.find_all("div", {"class": "layoutBox1 layoutBoxFull defaultTheme"})
-    
     for item_type in all_items:
         c_type = item_type.find_all("h1", {"class": "topBar last layoutBoxTitle"})[0].text #gets all item catagory names
+        #print("c_type", c_type)
+        clothing_types.append(c_type)
         items = item_type.find_all("table", {"class": "itemDataTable"}) #gets ALL the raw data for each item class
         for item_data in items: # for each clothing class
             data = item_data.find_all("tr") #get the raw data
@@ -325,8 +379,10 @@ def get_clothes():
                 item = parse_clothing(item_data)
                 x = x+1
                 #input("Press Enter to continue...")
+    write_clothing_types(clothing_types)
 
-def get_jewelry(): #layout is different - implicit mods are on the same line
+def fetch_jewelry(): #layout is different - implicit mods are on the same line
+    jewelry_types = []
     resp = s.get(url_jewelry)
     soup = BeautifulSoup(resp.text, 'html.parser')
     jewelry = soup.find_all("tr") 
@@ -335,54 +391,53 @@ def get_jewelry(): #layout is different - implicit mods are on the same line
         a = y.find_all("td", {"class": "name"})
         if len(a) > 0:
             pass
-            #print (a[0].text)
-    print ("### END OF PRINTIONG ALL JEWELRY NAMES ###")
-        
+            #print (a[0].text)   
     all_items = soup.find_all("div", {"class": "layoutBox1 layoutBoxFull defaultTheme"})
     for item_type in all_items:
         j_type = item_type.find_all("h1", {"class": "topBar last layoutBoxTitle"})[0].text #gets all item catagory names
+        jewelry_types.append(j_type)
         items = item_type.find_all("table", {"class": "itemDataTable"}) #gets ALL the raw data for each item class
         for item_data in items: # for each clothing class
             data = item_data.find_all("tr") #get the raw data
             x = 2 #first two entries are table formatting aspects
             while x < len(data): # need to collect two 'tr' entries for each item, so use while loop
-                print("top of while loop")
                 item_data = []
                 raw_data = data[x].find_all("td")
                 item_data.append(raw_data)
                 parse_jewelry(item_data)
                 x = x+1
-                #input("Press Enter to continue...")
+    write_jewelry_types(jewelry_types)
 
-def get_prefixes(): #layout is different - implicit mods are on the same line
+def fetch_prefixes(): #layout is different - implicit mods are on the same line
+    prefix_types = []
     resp = s.get(url_prefixes)
     soup = BeautifulSoup(resp.text, 'html.parser')
-    print(soup)
     prefixes = soup.find_all("tr") 
     prefixes[0].find_all("td", {"class": "name"})
     for y in prefixes:
         a = y.find_all("td", {"class": "name"})
         if len(a) > 0:
             pass
-            #print (a[0].text)
-    print ("### END OF PRINTIONG ALL JEWELRY NAMES ###")
     all_items = soup.find_all("div", {"class": "layoutBox1 layoutBoxFull defaultTheme"})
     for item_type in all_items:
-        j_type = item_type.find_all("h1", {"class": "topBar last layoutBoxTitle"})[0].text #gets all item catagory names
+        p_type = item_type.find_all("h1", {"class": "topBar last layoutBoxTitle"})[0].text #gets all item catagory names
+        prefix_types.append(p_type)
         items = item_type.find_all("table", {"class": "itemDataTable"}) #gets ALL the raw data for each item class
         for item_data in items: # for each clothing class
             data = item_data.find_all("tr") #get the raw data
             x = 1 #first two entries are table formatting aspects
             while x < len(data): # need to collect two 'tr' entries for each item, so use while loop
-                print("top of while loop")
                 item_data = []
                 raw_data = data[x].find_all("td")
                 item_data.append(raw_data)
-                parse_prefixes(item_data)
+                prefix = parse_prefixes(item_data)
+                print(p_type, prefix)
                 x = x+1
-                #input("Press Enter to continue...")
+    write_prefix_types(prefix_types)
+    
  
-def get_suffixes(): #layout is different - implicit mods are on the same line
+def fetch_suffixes(): #layout is different - implicit mods are on the same line
+    suffix_types = []
     resp = s.get(url_suffixes)
     soup = BeautifulSoup(resp.text, 'html.parser')
     suffixes = soup.find_all("tr") 
@@ -392,25 +447,27 @@ def get_suffixes(): #layout is different - implicit mods are on the same line
         if len(a) > 0:
             pass
             #print (a[0].text)
-    print ("### END OF PRINTIONG ALL JEWELRY NAMES ###")
     all_items = soup.find_all("div", {"class": "layoutBox1 layoutBoxFull defaultTheme"})
     for item_type in all_items:
-        j_type = item_type.find_all("h1", {"class": "topBar last layoutBoxTitle"})[0].text #gets all item catagory names
+        s_type = item_type.find_all("h1", {"class": "topBar last layoutBoxTitle"})[0].text #gets all item catagory names
+        suffix_types.append(s_type)
         items = item_type.find_all("table", {"class": "itemDataTable"}) #gets ALL the raw data for each item class
         for item_data in items: # for each clothing class
             data = item_data.find_all("tr") #get the raw data
             x = 1 #first two entries are table formatting aspects
             while x < len(data): # need to collect two 'tr' entries for each item, so use while loop
-                print("top of while loop")
                 item_data = []
                 raw_data = data[x].find_all("td")
                 item_data.append(raw_data)
                 parse_suffixes(item_data)
-                x = x+1              
+                x = x+1
+    write_suffix_types(suffix_types)     
  
-get_weapons()
-#get_clothes()
-#get_jewelry()
-#get_prefixes()
-#get_suffixes()
+ 
+fetch_prefixes()
+fetch_suffixes()
+fetch_weapons()
+fetch_clothes()
+fetch_jewelry()
+
 
