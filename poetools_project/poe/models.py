0b46a15587515cdd1e6f190664b4fe0434087f0e
Adam Green <adam.green@ocado.com>
2017-01-28 17:02:37 +0000

    added prefix models back into model.py

--- poetools_project/poe/models.py
+++ poetools_project/poe/models.py
@@ -85,6 +85,36 @@ class FixName(models.Model):
         db_table = 'fix_name'
         app_label = "poe"
 
+   
+class PrefixNames(models.Model):
+    name = models.CharField(unique=True, max_length=50)
+
+    class Meta:
+        managed = True
+        db_table = 'prefix_names'
+        app_label = "poe"
+
+
+class PrefixTypes(models.Model):
+    type = models.CharField(unique=True, max_length=50)
+
+    class Meta:
+        managed = True
+        db_table = 'prefix_types'
+        app_label = "poe"
+
+class Prefixes(models.Model):
+    type = models.ForeignKey(PrefixTypes, models.DO_NOTHING)
+    name = models.ForeignKey(PrefixNames, models.DO_NOTHING)
+    i_level = models.IntegerField()
+    crafted = models.BooleanField()
+    stat = models.ForeignKey('Stats', models.DO_NOTHING)
+
+    class Meta:
+        managed = True
+        db_table = 'prefixes'
+        app_label = "poe"
+        unique_together = (('type', 'name', 'i_level', 'crafted', 'stat'),)
 
 class StatNames(models.Model):
     name = models.CharField(unique=True, max_length=60)
