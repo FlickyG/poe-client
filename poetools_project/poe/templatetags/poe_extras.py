279802bed54310f40f375e12cf5508fc77221cce
Adam Green <adam.green@ocado.com>
2017-02-14 02:41:51 +0000

    half way through sectio 14, not done parameterised tags.  Test work

--- poetools_project/poe/templatetags/poe_extras.py
+++ poetools_project/poe/templatetags/poe_extras.py
@@ -1,8 +1,14 @@
 from django import template
-from poe.models import Category
+from poe.models import ItemCategory, FixCategory
 
 register = template.Library()
 
-@register.inclusion_tag('rango/cats.html')
-def get_category_list():
-    return {'cats': Category.objects.all()}
+@register.inclusion_tag('poe/cats.html')
+def get_item_category_list():
+    return {'item_cats': ItemCategory.objects.all()}
+
+@register.inclusion_tag('poe/fixes.html')
+def get_fix_category_list():
+    return {'fix_cats': FixCategory.objects.all()}
+
+
