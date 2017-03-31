558bdd8f29822c248d53822fe3c4e42988996efa
Adam Green <adam.green@ocado.com>
2017-03-31 09:19:21 +0100

    grabs character info but not tested

--- poetools_project/poe/common/character_tools.py
+++ poetools_project/poe/common/character_tools.py
@@ -23,27 +23,51 @@ s.hooks = {'response': make_throttle_hook(0.1)}
 
 
 def get_characters(poe_account):
-    stdlogger.debug("hello from get_characters")
-    print("hello world 2", poe_account.acc_name, poe_account.sessid)
+    stdlogger.debug("entering get_characters")
     # grab current character names from daabase
     saved_chars = poe.models.PoeCharacter.objects.all()
     saved_charnames = set([char.name for char in saved_chars])
-    print("saved.charnames", saved_charnames)
     #get character names on the server right now
     ggg_url = "https://www.pathofexile.com/character-window/get-characters"
     resp = s.get(ggg_url, cookies = {'POESESSID': poe_account.sessid})
     data = resp.json()
     live_charnames = set([line['name'] for line in data])
-    print("live_charnames", live_charnames)
-    live_subs_saved = live_charnames.issubset(saved_charnames)
-    saved_subs_live = saved_charnames.issubset(live_charnames)
     # delete stale characters
     # in saved_charnames but not in live_charnames
-    x = saved_charnames.difference(live_charnames)
-    print("x", x)
+    delete_chars = saved_charnames.difference(live_charnames)
+    for char in saved_chars:
+        if char.name in delete_chars: 
+            print("character names that need to be deleted", char)
+            char.delete()
     # insert new characters
     # in live_charnames but not in saved_charnames
     add_chars = live_charnames.difference(saved_charnames)
-    for char in add_chars:
-        print("char", char)
-    pass
+    for char in data:
+        if char['name'] in add_chars:
+            stdlogger.debug(' '.join(("Need to create this character", char['name'])))
+            new_char = poe.models.PoeCharacter(
+                        account = poe_account,
+                        name =char['name'],
+                        level = char['level'],
+                        ggg_class = char['class'],
+                        ascendancy_class = char['ascendancyClass'],
+                        classId = char['classId'],
+                        league = char['league'], 
+                        )
+            new_char.save()
+
+def delete_all_characters():
+    all_chars = poe.models.PoeCharacter.objects.all()
+    for char in all_chars:
+        stdlogger.debug(' '.join(("Deleting this character", char.name)))
+        char.delete()
+
+def get_items(poe_account, character):
+    print("hello")
+    equiped_items_URL = ("http://www.pathofexile.com/character-window/get-items?"
+                     "character={char}&accountName={acc}"
+                     .format(char = character.name, acc = poe_account.acc_name))
+    resp = s.get(equiped_items_URL, cookies = {'POESESSID': poe_account.sessid})
+    equipment = resp.json()
+    return equipment
+
