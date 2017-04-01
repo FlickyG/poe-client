30e1c00cbde33b69ef8c40342b2ef2a0cc0383e8
adam.green <adam.green@ocado.com>
2017-04-01 16:35:08 +0100

    datadump fixed, tests fail

--- poetools_project/poe/routers.py
+++ poetools_project/poe/routers.py
@@ -2,24 +2,24 @@
 
 class PoeRouter(object): 
     def db_for_read(self, model, **hints):
-        print("hello from PoeRouter")
+        #print("hello from PoeRouter")
         if model._meta.model_name == 'poeuser':
             return 'default'
         elif model._meta.app_label == 'poe':            
             return 'poe_db'
         #if model._meta.app_label == 'poe_auth':
-        #    print("app label ", model._meta.app_label)
+        #    #print("app label ", model._meta.app_label)
         #    return 'default'
         else:
             return 'default'
     
     def db_for_write(self, model, **hints):
-        print("hello from PoeRouter db_for_write")
+        #print("hello from PoeRouter db_for_write")
         return self.db_for_read(model, **hints)
 
     
     def allow_relation(self, obj1, obj2, **hints):
-        print("hello from PoeRouter allow_realtion")
+        #print("hello from PoeRouter allow_realtion")
         if obj1._meta.app_label == 'poe' or \
             obj2._meta.app_label == 'poe':
             return True
@@ -27,10 +27,10 @@ class PoeRouter(object):
             return None
         
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        print("hello from PoeRouter allow_migration")
+
         if model_name != 'poeuser' and app_label == 'poe':
-            print("poe_db" == db, "model_name", model_name, "app_label", app_label)
+            #print("poe_db" == db, "model_name", model_name, "app_label", app_label)
             return 'poe_db' == db
         else:
-            print("default" == db, "model_name", model_name, "app_label", app_label)
+            #print("default" == db, "model_name", model_name, "app_label", app_label)
             return 'default' == db 
